8f5497b0577aaf768074b67b730b7f051ffc3401
Abhishek Gupta <107456489+AbhishekGupta1212@users.noreply.github.com>
2024-05-03 15:51:05 +0530

    done

--- products/src/App.js
+++ products/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="*" element={"404 NOT FOUND"} />
         </Routes>
       </Router>
     </>
